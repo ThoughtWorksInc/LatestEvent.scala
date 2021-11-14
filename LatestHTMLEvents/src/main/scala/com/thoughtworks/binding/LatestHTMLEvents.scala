e1631039dd730b97b2dce3b8cc614397037c1c4e
Yang, Bo <pop.atry@gmail.com>
2021-11-14 03:18:20 -0800

    Add Scaladoc as the test

--- LatestHTMLEvents/src/main/scala/com/thoughtworks/binding/LatestHTMLEvents.scala
+++ LatestHTMLEvents/src/main/scala/com/thoughtworks/binding/LatestHTMLEvents.scala
@@ -8,6 +8,38 @@ import org.scalajs.dom.Element
 import org.lrng.binding.html.AttributeFactory
 import com.thoughtworks.binding.bindable.Bindable
 
+/** Including [[LatestHTMLEvents.of]], which automatically infers the event type based on the type class defined in
+  * [[org.lrng.binding.html]]
+  *
+  * @example
+  *   Given an HTML button element,
+  *   {{{
+  *   import org.lrng.binding.html, html.autoImports.xml.attributes
+  *   import org.scalajs.dom.MouseEvent
+  *   @html val myButton = <button>Click me!</button>
+  *   }}}
+  *
+  *   when creating the latest click event binding with the help of [[LatestHTMLEvents.of]],
+  *
+  *   {{{
+  *   val latestClickEvent = LatestHTMLEvents.of(myButton.value, attributes.onclick)
+  *   }}}
+  *
+  *   the event type should be inferred as [[org.scalajs.dom.MouseEvent]],
+  *
+  *   {{{
+  *   val _ = latestClickEvent: LatestEvent[MouseEvent]
+  *   }}}
+  *
+  *   and the `MouseEvent` properties, like `clientX`, can be used in the derived [[Binding]]s
+  *
+  *   {{{
+  *   def clientXBinding = Binding {
+  *     val mouseEvent: Option[MouseEvent] = latestClickEvent.bind
+  *     mouseEvent.map(_.clientX)
+  *   }
+  *   }}}
+  */
 object LatestHTMLEvents {
 
   def of[E <: org.scalajs.dom.Element, AttributeObject <: AttributeFactory.Typed, InferredEvent <: Event](
